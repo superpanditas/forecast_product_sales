755ee63bdf5d7ae153b14f9c28c1ae9449ff63f2
Vazzi <panditas.datascientist@gmail.com>
2024-12-09 14:32:46 -0600

    first commit

---
+++ .ipynb_checkpoints/test-checkpoint.py
@@ -0,0 +1,5 @@
+import pandas as pd 
+import numpy as np
+
+sales = pd.read_csv('/Users/mbp/Downloads/dataset_ventas-2.csv')
+print(sales.info())
---
+++ test.py
@@ -0,0 +1,6 @@
+import pandas as pd 
+import numpy as np
+
+sales = pd.read_csv('/Users/mbp/Downloads/dataset_ventas-2.csv')
+print(sales.info())
+
