9900aac51169001c861e33e48b7e054a82518d14
Vazzi <panditas.datascientist@gmail.com>
2024-12-09 16:07:33 -0600

    update dataset

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
+# Importing libraries
 import pandas as pd 
 import numpy as np
 
-sales = pd.read_csv('/Users/mbp/Downloads/dataset_ventas-2.csv')
-print(sales.info())
+# Creating dataframes 
+data = {
+    "Name":["Alice", "Bob", "Charlie", "David"],
+    "Age":[24, 27, 22, 32],
+    "City":["New York", "Los Angeles", "Chicago", "Houston"]
+}
+
+df = pd.DataFrame(data)
 
